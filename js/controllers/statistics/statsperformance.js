9dda307a5952b22208d8b857c64128e090730139
Frank Bassard <f.bassard@gmail.com>
2015-02-19 21:52:07 +1300

    Stats graph

--- js/controllers/statistics/statsperformance.js
+++ js/controllers/statistics/statsperformance.js
@@ -7,22 +7,53 @@ app.controller('StatsPerformanceCtrl', [
 	, function ($scope, Users, Exercices) {
 
 		$scope.init = function(){
-			Users.allExercices({
-				user_id : 1
-			}, function(data){
-				$scope.exercices = data;
+			$scope.exercices = Users.allExercices({user_id : 1}, function(){
 				$scope.selectedExercice = $scope.exercices[0];
-				$scope.changeExercice($scope.selectedExercice);				
+				$scope.changeExercice();
 			});
 		};
 
-		$scope.changeExercice = function(exercice){
-			Users.allSchedulesExercices({
+		$scope.changeExercice = function(){
+			$scope.records = Users.allSchedulesExercices({
 				user_id : 1,
-				exercice_id : exercice.id
-			}, function(data){
-
+				exercice_id : $scope.selectedExercice.id
 			});
+			plot();
 		};
+
+		function plot(){
+			Chart.defaults.global.responsive = true;
+			var ctx = document.getElementById("chart").getContext("2d");
+			var data = {
+		    labels: ["January", "February", "March", "April", "May", "June", "July"],
+		    datasets: [
+			        {
+			            label: "My First dataset",
+			            fillColor: "rgba(220,220,220,0.2)",
+			            strokeColor: "rgba(220,220,220,1)",
+			            pointColor: "rgba(220,220,220,1)",
+			            pointStrokeColor: "#fff",
+			            pointHighlightFill: "#fff",
+			            pointHighlightStroke: "rgba(220,220,220,1)",
+			            data: [65, 59, 80, 81, 56, 55, 40]
+			        },
+			        {
+			            label: "My Second dataset",
+			            fillColor: "rgba(151,187,205,0.2)",
+			            strokeColor: "rgba(151,187,205,1)",
+			            pointColor: "rgba(151,187,205,1)",
+			            pointStrokeColor: "#fff",
+			            pointHighlightFill: "#fff",
+			            pointHighlightStroke: "rgba(151,187,205,1)",
+			            data: [28, 48, 40, 19, 86, 27, 90]
+			        }
+			    ]
+			};
+
+			var myLineChart = new Chart(ctx).Line(data, {});
+			width = document.getElementById("chart").parentNode.offsetWidth;
+			document.getElementById("chart").style.width = width;
+			document.getElementById("chart").style.height = width/2;
+		}
 
 }]);
