4cbc9ce6347e8c8d958430c4d617eabc5b911af9
Frank Bassard <f.bassard@gmail.com>
2015-02-25 14:45:06 +1300

    Step : areas stats

--- js/controllers/statistics/statsareas.js
+++ js/controllers/statistics/statsareas.js
@@ -45,18 +45,52 @@ app.controller('StatsAreasCtrl', [
 		function plot(){
 			var ctx = document.getElementById("chart").getContext("2d"),
 				datas = [[]],
-				labels = [];
+				labels = [],
+				areaCount = 0, avg = 0, std = 0, min = 1000, max = 0;
 
 			for (var i = 0; i < $scope.areas.length; i++) {
 				if($scope.areas[i].selected){
+					areaCount++;
+					var trainingCount = getNumerOftraining($scope.areas[i]);
+					avg += trainingCount;
 					labels.push($scope.areas[i].name);
-					datas[0].push(getNumerOftraining($scope.areas[i]));
+					datas[0].push(trainingCount);
+					max = max > trainingCount ? max : trainingCount;
+					min = min < trainingCount ? min : trainingCount;
 				}
 			}
 
-			console.log(datas);
+			avg /= areaCount;
+
+			for (var j = 0; j < $scope.areas.length; j++) {
+				if($scope.areas[j].selected){
+					var trainingCount = getNumerOftraining($scope.areas[j]);
+					std += Math.pow(trainingCount - avg, 2);
+				}
+			}
+
 			var data = chartService.getData(labels, datas);
 			var myRadarChart = new Chart(ctx).Radar(data, {});
+
+			$scope.generalData = [{
+				title : "Data count",
+				data : $scope.exercices.length,
+				hr : true
+			}, {
+				title : "Average wourkouts count",
+				data : Math.round(avg * 100) / 100
+			}, {
+				title : "Std wourkouts count",
+				data : Math.round(Math.sqrt(std) * 100) / 100
+			}];
+
+			$scope.extremeData = [{
+				title : "Maximum",
+				data : max
+			}, {
+				title : "Minimum",
+				data : min
+			}];
 
 		}
 
