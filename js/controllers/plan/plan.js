d31a475d6536ad1ccf2ab8b45689d024dd1ccde1
Frank Bassard <f.bassard@gmail.com>
2015-03-12 21:16:37 +1300

    init Calendar

--- js/controllers/plan/plan.js
+++ js/controllers/plan/plan.js
@@ -3,5 +3,17 @@ app.controller('PlanController', [
 
 	function ($scope) {
 
+		$scope.init = function(){
+			$scope.days = [];
+			$scope.months = [];
+			$scope.today = moment(new Date());
+			$scope.year = $scope.today.year().format('YYYY');
+			for (var i = 0; i < $scope.today.daysInMonth(); i++) {
+				$scope.days.push(moment().date(i));
+			}
+			for (var j = 0; j < 12; j++) {
+				$scope.months.push(moment().month(j).format('MMMM'));
+			}
+		};
 
 }]);
