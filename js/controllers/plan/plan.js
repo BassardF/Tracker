f746d5b3ddf20c0a6d09ef33789d423f99706079
Frank Bassard <f.bassard@gmail.com>
2015-03-13 20:59:35 +1300

    Plan more

--- js/controllers/plan/plan.js
+++ js/controllers/plan/plan.js
@@ -12,12 +12,27 @@ app.controller('PlanController', [
 
 			$scope.today = moment(new Date());
 			$scope.year = $scope.today.year();
+
+			var withinFirstWeek = true;
 			for (var i = 0; i < $scope.today.daysInMonth(); i++) {
-				$scope.days.push(moment().date(i));
+				var date = moment().date(i + 1);
+				date.withinFirstWeek = withinFirstWeek;
+				withinFirstWeek = withinFirstWeek && date.isoWeekday() !== 7;
+				$scope.days.push(date);
 			}
+
 			for (var j = 0; j < 12; j++) {
 				$scope.months.push(moment().month(j).format('MMMM'));
 			}
 		};
 
+		$scope.isDayInFirstLine = function(isoDay){
+			var firstLine = false;
+			for (var i = 0; i < $scope.days.length; i++) {
+				if($scope.days[i].isoWeekday() === isoDay){
+					firstLine = firstLine || $scope.days[i].withinFirstWeek;
+				}
+			}
+			return firstLine;
+		}
 }]);
