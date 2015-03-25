b079c23d5e84483fe4005e5c7ba4017ee994ce81
Frank Bassard <f.bassard@gmail.com>
2015-03-25 20:21:22 +1300

    Design

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -4,30 +4,6 @@ app.directive('navbar', function() {
 	};
 });
 
-app.directive('exercicesNavbar', function() {
-	return {
-		templateUrl: 'views/directives/exercicesnavbar.html'
-	};
-});
-
-app.directive('goalsNavbar', function() {
-	return {
-		templateUrl: 'views/directives/goalsnavbar.html'
-	};
-});
-
-app.directive('measuresNavbar', function() {
-	return {
-		templateUrl: 'views/directives/measuresnavbar.html'
-	};
-});
-
-app.directive('statisticsNavbar', function() {
-	return {
-		templateUrl: 'views/directives/statisticsnavbar.html'
-	};
-});
-
 app.directive('dragSource', ['$rootScope', 'uuid', function($rootScope, uuid) {
     var directive = {};
 
